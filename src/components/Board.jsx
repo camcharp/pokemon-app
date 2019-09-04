319e51dba7354205f2cf93bc043536d14d4b301e
Camille Charpentier <cam.charpentier@live.fr>
2019-09-04 12:55:45 +0200

    created components Board + Tile

---
+++ src/components/Board.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Board extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Pokemon App</h1>
+            </div>
+        )
+    }
+}
